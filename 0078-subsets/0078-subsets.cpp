1a577311600b9f7e084c3255b99dd8244ad4d574
20Vidit04 <148935941+20Vidit04@users.noreply.github.com>
2023-11-01 18:13:41 +0530

    Time: 4 ms (38.45%), Space: 16.2 MB (30.35%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+   void solve(vector<int>& nums, vector<vector<int>> &ans, int index, vector<int> output){
+       if(index>=nums.size()){
+           ans.push_back(output);
+           return;
+       }
+      // exclude
+      solve(nums,ans,index+1,output);
+
+      int element=nums[index];
+      output.push_back(element);
+      solve(nums,ans,index+1,output);
+   }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index=0;
+        solve(nums,ans,index,output);
+        return ans;
+    }
+};
