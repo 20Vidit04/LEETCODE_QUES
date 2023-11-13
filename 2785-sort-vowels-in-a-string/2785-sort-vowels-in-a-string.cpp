1b23b08dbd051f9a52cef5a8b98859422b587054
20Vidit04 <148935941+20Vidit04@users.noreply.github.com>
2023-11-13 21:36:39 +0530

    Time: 48 ms (29.43%), Space: 16.9 MB (15.72%) - LeetHub

---
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+       
+     const string vowels="AEIOUaeiou";
+        string v;
+        vector<int> pos;
+        int n=s.size();
+        #pragma unroll
+        for(int i=0; i<n; i++){
+            if (vowels.find(s[i])!=-1){
+                v.push_back(s[i]);
+                pos.push_back(i);
+            }        
+        }
+        sort(v.begin(), v.end());
+        int count=0;
+        #pragma unroll
+        for(int i:pos){
+            s[i]=v[count++];
+        }
+      
+        return s;
+    }
+};
